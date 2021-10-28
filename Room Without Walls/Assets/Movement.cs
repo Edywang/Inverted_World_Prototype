7f737424177991c660344ab4f106de119637a0af
Edywang <edwinwang00@gmail.com>
2021-10-27 19:30:35 -0700

    Overhead Camera isn't ever rendering

--- Room Without Walls/Assets/Movement.cs
+++ Room Without Walls/Assets/Movement.cs
@@ -5,17 +5,13 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float speed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
+        //gameObject.CharacterController.move((speed), (speed));
         gameObject.transform.position = new Vector3(transform.position.x + (h * speed), transform.position.y, transform.position.z + (v * speed));
         if (transform.position.x > 15 || transform.position.x < -15){
             gameObject.transform.position = new Vector3(-transform.position.x, transform.position.y, transform.position.z);
