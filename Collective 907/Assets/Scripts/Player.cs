b7a863a295a293b4b0e1494fd4b8340d90762311
Edywang <edwinwang00@gmail.com>
2021-11-03 20:04:59 -0700

    Screw your typo

--- Collective 907/Assets/Scripts/Player.cs
+++ Collective 907/Assets/Scripts/Player.cs
@@ -9,6 +9,8 @@ public class Player : MonoBehaviour
     CharacterController characterController;
     GameObject planetCore;
     private Camera currentCamera, cam1, cam2;
+    private Vector3 moveDirection;
+    public Vector3 upDirection;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +20,8 @@ public class Player : MonoBehaviour
         currentCamera = Camera.main;
         cam1.enabled = true;
         cam2.enabled = false;
+        // Create a vector that is the character's orientation
+        moveDirection = transform.TransformDirection(Vector3.forward);
     }
 
     // FixedUpdate is called once per frame
@@ -26,12 +30,22 @@ public class Player : MonoBehaviour
         // Vector3 relativeToCore = transform.position - planetCore.transform.position;
 
         // Character movement
-        Vector3 movement = 
-            currentCameara.transform.right * Input.GetAxis("Horizontal") 
-            + currentCameara.transform.forward * Input.GetAxis("Vertical");
-        movement = Vector3.ClampMagnitude(movement, 1);
-        movement *= speed * Time.deltaTime;
-        characterController.SimpleMove(movement);
+        // Vector3 movement = 
+        //     currentCameara.transform.right * Input.GetAxis("Horizontal") 
+        //     + currentCameara.transform.forward * Input.GetAxis("Vertical");
+        // movement = Vector3.ClampMagnitude(movement, 1);
+        // movement *= speed * Time.deltaTime;
+        // characterController.SimpleMove(movement);
+
+        // Basic character movement based off character controller and moveDirection
+        if (characterController.isGrounded){
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed * Time.deltaTime;
+        }
+
+        // Set the up direction to the normalized vector from the player to the core
+        upDirection = transform.position - planetCore.transform.position;
 
         // Rotate
         // Vector3 cameraDirection = currentCameara.transform.forward;
