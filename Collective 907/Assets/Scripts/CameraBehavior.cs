68545be9b7dfafd4bc301452770da8556d10c536
Moises Perez <perezmoises132@gmail.com>
2021-11-05 11:59:39 -0700

    Player camera moves.

--- Collective 907/Assets/Scripts/CameraBehavior.cs
+++ Collective 907/Assets/Scripts/CameraBehavior.cs
@@ -8,9 +8,10 @@ public class CameraBehavior : MonoBehaviour
     public float rotateSpeed;
     public Vector3 offset;
 
-    public float mouseSensitivity = 100f;
+    public float mouseSensitivity = 1f;
     public Transform playerBody;
     float xRotation = 0f;
+    float yRotation = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,14 +22,15 @@ public class CameraBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX);
-
+        //transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(-mouseY, mouseX, 0);
+        //playerBody.Rotate(Vector3.up * mouseX);
+        
     }
     private void LateUpdate()
     {
