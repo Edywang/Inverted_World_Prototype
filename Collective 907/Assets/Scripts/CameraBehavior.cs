c177ea2dd18aa5eab752ca537b432bf93111b756
Edywang <edwinwang00@gmail.com>
2021-11-06 22:06:25 -0700

    Typo

--- Collective 907/Assets/Scripts/CameraBehavior.cs
+++ Collective 907/Assets/Scripts/CameraBehavior.cs
@@ -17,6 +17,6 @@ public class CameraBehavior : MonoBehaviour
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-        transform.rotate(mouxeY, mouseX, 0f);
+        transform.rotate(mouseY, mouseX, 0f);
     }
 }
