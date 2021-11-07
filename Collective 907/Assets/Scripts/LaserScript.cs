21cf21e02f35820741341e818cefe312d5696975
Edywang <edwinwang00@gmail.com>
2021-11-07 14:05:52 -0800

    Creating LineRenderer

--- Collective 907/Assets/Scripts/LaserScript.cs
+++ Collective 907/Assets/Scripts/LaserScript.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class LaserScript : MonoBehaviour
 {
+    private LineRenderer line;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3[] initLaserPositions = new Vector3[2] {Vector3.zero, Vector3.zero};
+        line = GetComponent<LineRenderer>();
+        line.SetPositions(initLaserPositions);
+        line.SetWidth(0.1f, 0.1f);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetLaserPositions(Vector3 start, RaycastHit objectHit)
     {
-        
+        Vector3 endPosition = objectHit.point;
+        Vector3[] laserPositions = new Vector3[2] {start, endPosition};
+        line.SetPositions(laserPositions);
+        // If the laser is not visible, make it visible
+        if (!line.enabled)
+        {
+            line.enabled = true;
+        }
     }
 }
