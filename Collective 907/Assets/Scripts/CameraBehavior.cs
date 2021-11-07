6b51f1ae4475ad3346e356a287180b7862a41a96
Edywang <edwinwang00@gmail.com>
2021-11-06 22:04:36 -0700

    Testing another camera implementation

--- Collective 907/Assets/Scripts/CameraBehavior.cs
+++ Collective 907/Assets/Scripts/CameraBehavior.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class CameraBehavior : MonoBehaviour
 {
     public float mouseSensitivity = 1f;
-    public Transform playerBody;
-    public Transform playerModel;
     float xRotation = 0f;
     // Start is called before the first frame update
     void Start()
@@ -17,14 +15,8 @@ public class CameraBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
-
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 50f);
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        //transform.Rotate(-mouseY, mouseX, 0);
-        playerBody.Rotate(Vector3.up * mouseX);
-        playerModel.Rotate(Vector3.up * mouseX);
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        transform.rotate(mouxeY, mouseX, 0f);
     }
 }
