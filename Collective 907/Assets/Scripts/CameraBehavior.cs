1560931f31dc8b3637f1092302761b1d8dd02f19
Edywang <edwinwang00@gmail.com>
2021-11-07 13:56:31 -0800

    Encapsulating shooting

--- Collective 907/Assets/Scripts/CameraBehavior.cs
+++ Collective 907/Assets/Scripts/CameraBehavior.cs
@@ -19,22 +19,26 @@ public class CameraBehavior : MonoBehaviour
     void Update()
     {
         if (Input.GetMouseButtonDown(0)) {
-            Debug.Log("clicked");
-            part.Play();
-            Ray ray = new Ray(transform.position, transform.forward);
-            //LineRenderer.DrawRay(transform.position, ray);
-            Debug.Log("ray: " + ray);
-            if(Physics.Raycast(ray, out RaycastHit hit)){
-                EnemyScript target = hit.collider.transform.gameObject.GetComponent<EnemyScript>();
-                Debug.Log("HIT: " + hit.collider.transform.gameObject.name);
-                if (target != null) {
-                    target.takeHit(damage);
-                }
-            }
+            Debug.Log("Clicked Primary Mouse Button");
+            ShootLaser(transform.position, transform.forward);
         }
         // float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         // float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
         // transform.Rotate(-mouseY, mouseX, 0f);
+    }
+
+    void ShootLaser(Vector3 startPosition, Vector3 direction){
+        part.Play();
+        Ray ray = new Ray(startPosition, direction);
+        //LineRenderer.DrawRay(transform.position, ray);
+        Debug.Log("ray: " + ray);
+        if(Physics.Raycast(ray, out RaycastHit hit)){
+            EnemyScript target = hit.collider.transform.gameObject.GetComponent<EnemyScript>();
+            Debug.Log("HIT: " + hit.collider.transform.gameObject.name);
+            if (target != null) {
+                target.takeHit(damage);
+            }
+        }
     }
     public void rotateHelper(Vector3 eulers, Space relativeTo = Space.Self)
     {
